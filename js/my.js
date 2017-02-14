099e182592eb3e957661516532c45301d2a7206a
Dima Esipenko <mif83@mail.ru>
2017-02-14 21:55:49 +0200

    functionality ready 100%

--- js/my.js
+++ js/my.js
@@ -19,35 +19,53 @@ Soldier.prototype.dropBackpack = function (){
 Soldier.prototype.buyBackpack = function(volume){
     this.backpack = new Backpack(volume);
 };
+Soldier.prototype.countWeight = function () {
+    var count = 0;
+    if (this.backpack){
+       count += this.backpack.items.reduce(function(sum, item){return sum+=item.weight}, 0);
+    };
+    if (this.bag){
+        count += this.bag.items.reduce(function(sum, item){return sum+=item.weight}, 0);
+    };
+    return count;
+};
 /***
 *   name: "knife", (String)
 *  weight: 1.1     (Number or string)
 ***/
 Soldier.prototype.putToBackpack = function(name, weight, bagKind){
-    var self = this[bagKind] || this.backpack;
+    var bagKind = bagKind || "backpack",
+        self = this[bagKind],
         item = {};
     item.name = name;
     item.weight = +weight;
-    if (self.items.length + 1 == self.volume)return console.warn("Backpack is full");
-    if ( this.power * 3 < self.items.reduce(function(sum, item){return sum+=item.weight}, 0) + item.weight ) return console.warn("too hard");
+
+    if (self.items.length + 1 == self.volume)return console.warn(bagKind + " is full");
+    if ( this.power * 3 < this.countWeight() + item.weight ) return console.warn("too hard");
+
     self.items.push(item);
     return JSON.stringify(self.items);
 };
 /***
 * itemName (String)
 ***/
-Soldier.prototype.getFromBackpack = function(itemName){
+Soldier.prototype.getFromBackpack = function(itemName, bagKind){
+    debugger;
     var count = 0,
-        self = this.backpack;
+        bagKind = bagKind || "backpack",
+        self = this[bagKind],
+        lastLength = self.items.length;
 
     self.items = self.items.filter(function (item){
-     if (item.name === itemName && count === 0){
-         count++;
-         return false;
-     }else {
-         return true;
-     }
-    });
+                                     if (item.name === itemName && count === 0){
+                                         count++;
+                                         return false;
+                                     }else {
+                                         return true;
+                                     }
+                                    });
+
+    if (lastLength == self.items.length) return "not such thing";
     return JSON.stringify(self.items);
 };
 Soldier.prototype.speech = function (str) {
@@ -65,6 +83,17 @@ function SuperSoldier(name, power, message){
 }
 SuperSoldier.prototype = Object.create(Soldier.prototype);
 
-SuperSoldier.prototype.putToBag = function(){
-
+SuperSoldier.prototype.putToBag = function(name, weight){
+    var kindBag = "bag";
+    return this.putToBackpack(name, weight, kindBag);
 };
+SuperSoldier.prototype.getFromBag = function (itemName) {
+    var kindBag = "bag";
+    return this.getFromBackpack(itemName, kindBag);
+};
+var squad = [];
+s1 = new Soldier();
+s2 = new SuperSoldier();
+squad.push(new Soldier());
+squad.push(new Soldier());
+squad.push(new SuperSoldier());
